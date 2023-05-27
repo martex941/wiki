47e1499c2852ea222b6836c4ad6eb46ebb271bc7
K_W <90504365+martex941@users.noreply.github.com>
2023-05-27 16:20:48 +0200

    Implemented entry page and index page.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("no_entry", views.no_entry, name="no_entry"),
+    path("<str:entry_name>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+import markdown2
 
 from . import util
 
 
 def index(request):
+
+    if request.method == "POST":
+        query = request.POST.get("q")
+        return HttpResponseRedirect(query)
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def no_entry(request):
+    return render(request, "encyclopedia/no_entry.html")
+
+def entry(request, entry_name):
+
+    try:
+        current_entry = util.get_entry(entry_name)
+        current_entry = markdown2.markdown(current_entry)
+    except TypeError:
+        return HttpResponseRedirect("no_entry")
+
+    return render(request, "encyclopedia/entry.html", {
+        "current_entry": current_entry
     })
 
