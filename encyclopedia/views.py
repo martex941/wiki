bb68abe5ce2b5a422ca33ab4e5f5e332405a20a3
K_W <90504365+martex941@users.noreply.github.com>
2023-05-28 15:32:07 +0200

    Implemented search entry funciton.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse
 from django.http import HttpResponseRedirect
+import re
 import markdown2
 
 from . import util
@@ -8,19 +8,25 @@ from . import util
 
 def index(request):
 
+    entries = util.list_entries()
+
     if request.method == "POST":
         query = request.POST.get("q")
-        return HttpResponseRedirect(query)
-
+        if query in entries:
+            return HttpResponseRedirect(query)
+        else:
+            pattern = rf'.*{query}.*'
+            matched_entries = [item for item in entries if re.match(pattern, item, re.IGNORECASE)]
+            return render(request, "encyclopedia/search_results.html", {"matched_entries":matched_entries, "query": query})
+        
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
 def no_entry(request):
     return render(request, "encyclopedia/no_entry.html")
 
 def entry(request, entry_name):
-
     try:
         current_entry = util.get_entry(entry_name)
         current_entry = markdown2.markdown(current_entry)
@@ -31,3 +37,5 @@ def entry(request, entry_name):
         "current_entry": current_entry
     })
 
+
+    
